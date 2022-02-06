b498e8130521f5d7348f89653aee6dd14c90e275
Stalkon <anvrkey@yandex.ru>
2022-02-07 02:05:43 +0300

    Добавлен ответ на вопрос "Почему я хочу стать программистом?"

--- CealnetCode.HelloWorld.CLL/Program.cs
+++ CealnetCode.HelloWorld.CLL/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Почему я хочу стать программистом?");
+Console.WriteLine("Люблю борется с монотонной ручной работой и автоматизировать данные процессы.");
+
