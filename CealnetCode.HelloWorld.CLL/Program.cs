c0dbec54158009df269431cb9f88900720966f40
Stalkon <anvrkey@yandex.ru>
2022-02-09 21:49:46 +0300

    Добавил, ответ на фопрос что для меня програмирование.

--- CealnetCode.HelloWorld.CLL/Program.cs
+++ CealnetCode.HelloWorld.CLL/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+Console.WriteLine("Как бы странно не прозвучало, но программирования для меня чем-то даже творчество.");
 Console.WriteLine("Почему я хочу стать программистом?");
 Console.WriteLine("Люблю борется с монотонной ручной работой и автоматизировать данные процессы.");
+
 
